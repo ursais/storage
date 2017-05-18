78c909ae9c180aabcc8949e8f7bc9e656fcd0d8a
Sébastien BEAU <sebastien.beau@akretion.com>
2017-05-18 14:31:21 +0200

    [IMP] remove dead code, start to refactor amazon export, fix issue with uploading image with res_id/res_model, fix issue with the creation of the backend

--- storage_backend/models/__init__.py
+++ storage_backend/models/__init__.py
@@ -4,4 +4,8 @@ from . import storage_backend
 from . import local_backend
 from . import sftp_backend
 from . import odoo_backend
+<<<<<<< HEAD
+=======
+from . import s3_backend
+>>>>>>> [IMP] remove dead code, start to refactor amazon export, fix issue with uploading image with res_id/res_model, fix issue with the creation of the backend
 from . import keychain
