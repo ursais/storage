82de802df8954d10915b1bcee647ace4d6d19a0e
Sébastien BEAU <sebastien.beau@akretion.com>
2018-04-09 09:46:07 +0200

    [REF] start to refactor code. Start to use component instead of odoo class, always build an url even if served by odoo. WIP

--- storage_backend/models/__init__.py
+++ storage_backend/models/__init__.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import storage_backend
-from . import local_backend
-from . import sftp_backend
-from . import odoo_backend
-<<<<<<< HEAD
-=======
-from . import s3_backend
->>>>>>> [IMP] remove dead code, start to refactor amazon export, fix issue with uploading image with res_id/res_model, fix issue with the creation of the backend
 from . import keychain
