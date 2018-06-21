6525bf9c29833f69805f85a6ed51ee7c1dac53b2
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-06-21 17:25:51 +0200

    Add connection test

--- storage_backend_sftp/models/storage_backend.py
+++ storage_backend_sftp/models/storage_backend.py
@@ -1,7 +1,16 @@
 # Copyright 2017 Akretion (http://www.akretion.com).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+import logging
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+logger = logging.getLogger(__name__)
+try:
+    import paramiko
+except ImportError as err:
+    logger.error(err)
 
 
 class StorageBackend(models.Model):
@@ -23,3 +32,32 @@ class StorageBackend(models.Model):
         string='Port',
         default=22,
         sparse="data")
+
+    def _get_stfp_connection(self):
+        try:
+            account = self._get_keychain_account()
+            password = account.get_password()
+            transport = paramiko.Transport(
+                    (self.sftp_server, self.sftp_port))
+            transport.connect(username=self.sftp_login, password=password)
+        except Exception as e:
+            raise Warning(_('Exception details\n\n%s' % e))
+        return transport
+
+    def _close_stfp_connection(self, transport):
+        try:
+            transport.close()
+        except Exception as e:
+            raise Warning(_('Exception details\n\n%s' % e))
+        return
+
+    @api.multi
+    def connnection_test(self):
+        self.ensure_one()
+        try:
+            transport = self._get_stfp_connection()
+            self._close_stfp_connection(transport)
+        except Exception as e:
+            raise Warning(_('Exception details\n\n%s' % e))
+        # Not a user error but a quick way to send feedback
+        raise UserError(_('Test connection successful.'))
